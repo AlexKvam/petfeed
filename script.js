4ffc553f6b95e7afa0638fdd0a27f913079550c7
AlexKvam <38686554+AlexKvam@users.noreply.github.com>
2023-07-25 23:27:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,37 +10,37 @@ function fetchData() {
     // You can use XMLHttpRequest, Fetch API, or any JavaScript framework (e.g., jQuery) for this purpose.
     // The data can be fetched from the PHP server using JSON format or any other suitable format.
     // Sample code:
-    // var xhr = new XMLHttpRequest();
-    // xhr.onreadystatechange = function() {
-    //     if (xhr.readyState === XMLHttpRequest.DONE) {
-    //         if (xhr.status === 200) {
-    //             var data = JSON.parse(xhr.responseText);
-    //             document.getElementById("currentTime").textContent = "Current Time: " + data.currentTime;
-    //             document.getElementById("feedingTime").textContent = "Feeding Time: " + data.feedingTime;
-    //         } else {
-    //             console.error("Failed to fetch data.");
-    //         }
-    //     }
-    // };
-    // xhr.open("GET", "server.php?action=getData", true);
-    // xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                document.getElementById("currentTime").textContent = "Current Time: " + data.currentTime;
+                document.getElementById("feedingTime").textContent = "Feeding Time: " + data.feedingTime;
+            } else {
+                console.error("Failed to fetch data.");
+            }
+        }
+    };
+    xhr.open("GET", "server.php?action=getData", true);
+    xhr.send();
 }
 
 function triggerFeeding() {
     // Trigger the feeding process on the server using AJAX or Fetch API.
     // Sample code:
-    // var xhr = new XMLHttpRequest();
-    // xhr.onreadystatechange = function() {
-    //     if (xhr.readyState === XMLHttpRequest.DONE) {
-    //         if (xhr.status === 200) {
-    //             console.log("Feeding triggered successfully!");
-    //         } else {
-    //             console.error("Failed to trigger feeding.");
-    //         }
-    //     }
-    // };
-    // xhr.open("GET", "server.php?action=trigger", true);
-    // xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                console.log("Feeding triggered successfully!");
+            } else {
+                console.error("Failed to trigger feeding.");
+            }
+        }
+    };
+    xhr.open("GET", "server.php?action=trigger", true);
+    xhr.send();
 }
 
 function setNewFeedingTime() {
@@ -50,18 +50,18 @@ function setNewFeedingTime() {
 
     // Validate the input (you can add further validation if needed).
 
-    // Send the new feeding time to the server using AJAX or Fetch API.
-    // Sample code:
-    // var xhr = new XMLHttpRequest();
-    // xhr.onreadystatechange = function() {
-    //     if (xhr.readyState === XMLHttpRequest.DONE) {
-    //         if (xhr.status === 200) {
-    //             console.log("Feeding time set successfully!");
-    //         } else {
-    //             console.error("Failed to set feeding time.");
-    //         }
-    //     }
-    // };
-    // xhr.open("GET", "server.php?action=setTrigger&hour=" + newHour + "&minute=" + newMinute, true);
-    // xhr.send();
+    //Send the new feeding time to the server using AJAX or Fetch API.
+    //Sample code:
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                console.log("Feeding time set successfully!");
+            } else {
+                console.error("Failed to set feeding time.");
+            }
+        }
+     };
+     xhr.open("GET", "server.php?action=setTrigger&hour=" + newHour + "&minute=" + newMinute, true);
+     xhr.send();
 }
